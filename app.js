722d164c48c77da512cf2dd67cab7aea44057ccc
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-07-26 16:28:03 -0400

    Checking For Reverse Integer, Complete

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
 // take an array of integers and return how many integers in the array match the criteria. 
 
-function same ( arrayOfInts ) {
-	let count = 0;
-	let length = arrayOfInts.length 
-	let copy = [...arrayOfInts]
-	for (  let i = 0; i < length; i++ ) {
-		current = arrayOfInts[i]
-		copy.shift()
-		let copyLength = copy.length
-		for ( let j = 0; j < copyLength; j++ ) {
-			let copyCurrent = copy[j]
-			if ( copyCurrent === current ) {
-				count ++
+// function same ( arrayOfInts ) {
+// 	let count = 0;
+// 	let length = arrayOfInts.length 
+// 	let copy = [...arrayOfInts]
+// 	for (  let i = 0; i < length; i++ ) {
+// 		let current = arrayOfInts[i]
+// 		copy.shift()
+// 		let copyLength = copy.length
+// 		for ( let j = 0; j < copyLength; j++ ) {
+// 			let copyCurrent = copy[j]
+// 			if ( copyCurrent === current ) {
+// 				count ++
+// 			}
+// 		}
+// 	}
+// 	return count	
+// }
+
+function string( int ) {
+	let string = String(int)
+	string = string.split('')
+	let reverse = []
+	string.forEach ( num => reverse.unshift( num ) ) 
+	reverse = reverse.join('')
+	return parseInt(reverse)
+}
+
+function reverse ( arrayOfInts ) {
+		let count = 0;
+		let length = arrayOfInts.length
+		let copy = [...arrayOfInts]
+		for (  let i = 0; i < length; i++ ) {
+			let current = string(arrayOfInts[i])
+			copy.shift()
+			let copyLength = copy.length
+			for ( let j = 0; j < copyLength; j++ ) {
+				let copyCurrent = copy[j]
+				if ( copyCurrent === current ) {
+					count ++
+				}
 			}
 		}
-
-	}
-	return count
-	
-	
-	
-	
+		return count
 }
 
 
@@ -31,8 +53,10 @@ function same ( arrayOfInts ) {
 
 // the integers characters are the exact same as each other. 
 
-let arrayOfInts = [ 4000, 2100, 2250, 2250, 2312, 2132,];
+let arrayOfInts = [ 4000, 6783, 2100, 2250, 2250, 2312, 2132, 3876];
 
 //console.log(isSame( arrayOfInts[0] ) )
 
- console.log( same ( arrayOfInts ) )
+//  console.log( same ( arrayOfInts ) )
+
+console.log(reverse(arrayOfInts));
