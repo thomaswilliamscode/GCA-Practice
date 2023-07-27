1463077cbc864c1a44cc47b7c78e61fb7d1f4c26
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-07-27 10:49:04 -0400

    Checking all characters, Complete. This is the only function actually needed to return the count.

--- app.js
+++ app.js
@@ -18,31 +18,60 @@
 // 	return count	
 // }
 
-function string( int ) {
-	let string = String(int)
-	string = string.split('')
-	let reverse = []
-	string.forEach ( num => reverse.unshift( num ) ) 
-	reverse = reverse.join('')
-	return parseInt(reverse)
-}
+// function string( int ) {
+// 	let string = String(int)
+// 	string = string.split('')
+// 	let reverse = []
+// 	string.forEach ( num => reverse.unshift( num ) ) 
+// 	reverse = reverse.join('')
+// 	return parseInt(reverse)
+// }
 
-function reverse ( arrayOfInts ) {
-		let count = 0;
-		let length = arrayOfInts.length
-		let copy = [...arrayOfInts]
-		for (  let i = 0; i < length; i++ ) {
-			let current = string(arrayOfInts[i])
-			copy.shift()
-			let copyLength = copy.length
-			for ( let j = 0; j < copyLength; j++ ) {
-				let copyCurrent = copy[j]
-				if ( copyCurrent === current ) {
-					count ++
-				}
+// function reverse ( arrayOfInts ) {
+// 		let count = 0;
+// 		let length = arrayOfInts.length
+// 		let copy = [...arrayOfInts]
+// 		for (  let i = 0; i < length; i++ ) {
+// 			let current = string(arrayOfInts[i])
+// 			copy.shift()
+// 			let copyLength = copy.length
+// 			for ( let j = 0; j < copyLength; j++ ) {
+// 				let copyCurrent = copy[j]
+// 				if ( copyCurrent === current ) {
+// 					count ++
+// 				}
+// 			}
+// 		}
+// 		return count
+// }
+
+let count = 0
+function characters ( arrayOfInts ) {
+	let length = arrayOfInts.length
+	let copy = [...arrayOfInts]
+	let current = []
+	for ( let i = 0; i < length; i++ ) {
+		// push first int to current array 
+		current.push(copy[0]);
+		copy.shift()
+		let copyLength = copy.length
+		// split first integer in the array 
+		let split = String(current[i]).split('');
+		let splitSort = split.sort().join('');
+		// for loop to check the rest of the integers in the array 
+		for (let j = 0; j < copyLength; j++) {
+			let jSplit = String( copy[j] ).split('')
+			// reorder both integers i am looking at to be in order and see if those numbers match up with each other 
+			let jSort = jSplit.sort().join('')
+			if (splitSort === jSort) {
+				count++
 			}
+
 		}
-		return count
+	}
+	return count
+	
+	
 }
 
 
@@ -53,10 +82,14 @@ function reverse ( arrayOfInts ) {
 
 // the integers characters are the exact same as each other. 
 
-let arrayOfInts = [ 4000, 6783, 2100, 2250, 2250, 2312, 2132, 3876];
+// let arrayOfInts = [ 4000, 6783, 2100, 2250, 2250, 2312, 2132, 3876];
 
-//console.log(isSame( arrayOfInts[0] ) )
+let arrayOfInts = [ 1234, 2345, 2134, 1754, 4751,  ]
 
-//  console.log( same ( arrayOfInts ) )
+	//console.log(isSame( arrayOfInts[0] ) )
 
-console.log(reverse(arrayOfInts));
+	//  console.log( same ( arrayOfInts ) )
+
+	// console.log(reverse(arrayOfInts));
+
+	console.log(characters(arrayOfInts));
